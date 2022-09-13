feb4e5d96f05ec0d3418da29eec8e059932f8dd8
blackcoffepls <69984284+blackcoffepls@users.noreply.github.com>
2022-09-13 14:52:16 -0300

    .

--- 1-IMPERATIVO/Parciales/2020/2020.pas
+++ 1-IMPERATIVO/Parciales/2020/2020.pas
@@ -12,3 +12,152 @@ lista que contenga la cantidad total vendida para cada código de producto, orde
 código de producto. }
 
 program p2020;
+
+const 
+	maxV=200;
+	dimF=5;
+
+type
+	rango=1..dimF;
+	indice=0..dimF;
+	venta=record
+		cod: integer;
+		cant: integer;
+		monto: integer;
+	end;
+	
+	lista=^nodo;
+	nodo=record
+		dato:venta;
+		sig:lista;
+	end;
+	
+	TV=array[rango]of lista;
+		
+	ventaA=record
+		cod: integer;
+		cant: integer;
+	end;
+		
+	listaA=^nodoA;
+	nodoA=record
+		dato:ventaA;
+		sig:listaA;
+	end;
+
+procedure InsertarOrdenado(var L:lista; dato:venta);
+var nuevo, act, ant:lista;
+begin
+	new(nuevo);
+	nuevo^.dato:=dato;
+	act:=L;
+	ant:=L;
+	
+	while (act<>nil) and (act^.dato.cod<dato.cod) do begin
+		ant:=act;
+		act:=act^.sig;
+	end;
+	if ant=act then L:=nuevo
+	else ant^.sig:=nuevo;
+	nuevo^.sig:=act;
+end;
+
+procedure Generar(var V:TV);
+var dato:venta; i:rango; total:integer;
+begin
+	for i:=1 to dimF do begin
+		dato.cod:=random(5);
+		total:=0;
+		while (total<200) and (dato.cod<>-1) do begin
+			total:=total+1;
+			dato.cant:=random(10)+1;
+			dato.monto:=random(100)+1;
+			
+			InsertarOrdenado(V[i],dato);
+			
+			dato.cod:=random(5)-1;
+		end;
+	end;
+end;
+
+procedure MostrarLista(L:lista);
+begin
+	while L<>nil do begin
+		writeln('Codigo: ',L^.dato.cod,' | Cantidad: ',L^.dato.cant,' | Monto: ',L^.dato.monto);
+		L:=L^.sig;
+	end;
+end;
+
+procedure MostrarVector(V:TV);
+var i:rango;
+begin
+	for i:=1 to dimF do begin
+		writeln('Sucursal ',i);
+		MostrarLista(V[i]);writeln;
+	end;
+end;
+
+procedure MergeAcum(V:TV; var L:listaA);
+const BIG=9999;
+
+	procedure Minimo(var V:TV; var minimo:ventaA);
+	var minimoI,i:integer;
+	begin
+		//Busco el minimo en el vector
+		minimo.cod:=BIG;
+		for i:=1 to dimF do begin
+			if (V[i]<>nil) and (V[i]^.dato.cod<minimo.cod) then begin
+				minimo.cod:=V[i]^.dato.cod;
+				minimo.cant:=V[i]^.dato.cant;
+				minimoI:=i;
+			end;
+		end;
+		//Avanzo al elemento siguiente en la lista
+		if minimo.cod<>BIG then V[minimoI]:=V[minimoI]^.sig;
+	end;
+	
+	procedure AgregarAtras(var L,ult:listaA; dato:VentaA);
+	var nuevo:listaA;
+	begin
+		new(nuevo);nuevo^.dato:=dato;nuevo^.sig:=nil;
+		if L<>nil then ult^.sig:=nuevo
+		else L:=nuevo;
+		ult:=nuevo;
+	end;
+
+var min,actual:ventaA; ult:listaA;
+begin 
+	L:=nil;
+	Minimo(V,min);
+	while min.cod<>BIG do begin
+		actual.cod:=min.cod;
+		actual.cant:=0;
+		while (min.cod<>BIG) and (min.cod=actual.cod) do begin
+			actual.cant:=actual.cant+min.cant;
+			Minimo(V,min);
+		end;
+		AgregarAtras(L,ult,actual);
+	end;
+end;
+
+procedure MostrarLista(L:listaA);
+begin
+	while L<>nil do begin
+		writeln('Codigo: ',L^.dato.cod,' | Cantidad: ',L^.dato.cant);
+		L:=L^.sig;
+	end;
+end;
+
+var
+	V:TV;
+	LA:listaA;
+begin
+	Randomize;
+	Generar(V);
+	MostrarVector(V);writeln;
+	
+	MergeAcum(V,LA);
+	MostrarLista(LA);
+end.
+	
+	
