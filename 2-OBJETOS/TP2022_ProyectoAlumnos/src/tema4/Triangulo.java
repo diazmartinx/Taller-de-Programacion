75d62cc6118ca0dd60f14fc5e2a06c8f824fa009
blackcoffepls <69984284+blackcoffepls@users.noreply.github.com>
2022-10-12 19:27:03 -0300

    .

---
+++ 2-OBJETOS/TP2022_ProyectoAlumnos/src/tema4/Triangulo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tema4;
+
+/**
+ *
+ * @author Martín
+ */
+public class Triangulo extends Figura {
+    
+    
+    public double calcularArea(){
+        return 0;
+    }
+    public double calcularPerimetro(){
+        return 0;
+    }
+}
